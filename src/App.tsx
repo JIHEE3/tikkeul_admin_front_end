752ddbfa2d3cf413cf0c3c1cc2d39db82a7ee8d2
JIHEE LEE <jihee33777@gmail.com>
2022-04-25 13:17:21 +0900

    회원가입, snackbar 추가
    - 성별 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { deepPurple, indigo } from "@mui/material/colors";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
 import SignIn from "./components/pages/signIn";
 import SignUp from "./components/pages/signUp";
@@ -16,12 +17,14 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <SnackbarProvider maxSnack={3}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
